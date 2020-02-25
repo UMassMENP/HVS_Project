6507cf36cdb21faabff9b14cbcd214cc9b04acd5
UMassMENP <55410056+UMassMENP@users.noreply.github.com>
2020-02-25 14:46:28 -0500

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -1,15 +1,16 @@
 #GUI for the HVS
 #Please update the date and time here so we can keep track of the newest copy:
-#Version: Feb 24, 2020 15:27
-#Changelog: packs reformatted to grid, Entry now throws an error if the value exceeds range 0 to 3000
+#Version: Feb 25, 2020 12:52
 #TODO:
-#3)Begin implementing voltage ramp function
+#3)Continue implementing voltage ramp function
 #3a)All the print functions should be formatted to a text box
-#4)NOT URGENT - More comments
-#5)Format py file to executable
-from tkinter import *
-import tkinter as tk
-#from voltage_ramp import *
+#3b)Implement functions and prep for hardware test
+#5)Format py file to executable - last step
+#6)NOT URGENT - ENTER/RETURN key functionality
+#7)Scroll bar to text box for ease
+from Tkinter import *
+import Tkinter as tk
+#from voltage_ramp import * #do not uncomment until hardware test
 
 mainWindow = Tk()
 #master window class
@@ -22,9 +23,6 @@ class Window(Frame):
         self.init_window()#main window and menu bar
 
         self.main_widgets()#objects that inhabit the main page
-#global variable declarationshow to import functions from another python file
-        self.errlbl = None
-        self.errlbl2 = None
 # use this function for menu bar and page title
     def init_window(self):
         self.master.title("HVS")
@@ -33,16 +31,17 @@ class Window(Frame):
         #this only works if you keep the variable name as "menu"
         menu = Menu(mainWindow)
         mainWindow.config(menu = menu)
-
-        #file cascade menu
+#all menu functions require the command passed from a seperate function
+        #file cascade menu 
         file_C = Menu(menu)
         file_C.add_command(label='Exit', command=self.close_window)
         menu.add_cascade(label='File', menu=file_C)
+
 #this function defines the main page controls
     def main_widgets(self):
         #ramp voltage entry
         Label(mainWindow, text='Enter Desired Voltage: ').grid(row = 0)
-        #Voltage Entry bar 
+        #Voltage Entry user input
         self.v_Entry = Entry(mainWindow)
         self.v_Entry.grid(row = 0, column = 1)
         #Button to pass entry to ramp voltage function, as to not cause lag
@@ -51,19 +50,24 @@ class Window(Frame):
         #Text box that will read out what used to be printed to console
         self.text_box = Text(mainWindow,height=15,width=45)
         self.text_box.grid(row=3,column=1)
-        self.text_box.insert(tk.END,'-----\nVoltage set to 0V...\n')
-        self.text_box.insert(tk.END,'-----\nMax Current Set to 3.3 mA\n')
-        self.text_box.insert(tk.END,'-----\n\n')
+        self.text_box.insert(tk.INSERT,'-----\n\n')
         self.text_box.configure(state='disabled')
 
 
-#voltage ramp function 
+#voltage ramp function
     def r_Entry(self, goalVoltage):
         print(goalVoltage)
+        voltage_ramp(goalVoltage)
+        self.text_box.configure(state = "normal")
+        self.text_box.insert(tk.END,'Voltage start at 0 V...\n')
+        self.text_box.insert(tk.END,'-----------\n')
+        self.text_box.insert(tk.END,'Max Current Set to 3.3 mA\n')
+        self.text_box.insert(tk.END,'-----------\n')
+        self.text_box.configure(state='disabled')
+        
 
 #this function will check if the entered value is an int, float, etc. then pass
 #to voltage ramp function
-#TODO: throw error if voltage exceeds the range of 0 - 3000 volts
     def ramp_Entry_Check(self):
         rampV = self.v_Entry.get()
         check = None
@@ -75,32 +79,27 @@ class Window(Frame):
                 rampV = float(rampV)
                 check = True
             except:#if the input is not a float or int, it fails
-
-                self.text_box.configure(state='normal')
-                self.text_box.insert(tk.INSERT,'Error: Entry must be an Integer or Float\n')
-                self.text_box.insert(tk.INSERT, '----------------\n')
-                self.text_box.configure(state='disabled')
+                self.text_box.configure(state='normal')#to add config must be turned on
+                self.text_box.insert(tk.END,'Error: Entry must be an Integer or Float\n')
+                self.text_box.insert(tk.END, '----------------\n')
+                self.text_box.configure(state='disabled')#turned off to prevent user input in text box
                 check = False
-        
-        if check == True:
+        if check == True:#check if voltage is in the desired range
             if rampV < 0 or rampV > 3000:
                 self.text_box.configure(state='normal')
-                self.text_box.insert(tk.INSERT, "Error: Entry must be between 0V and 3000V\n")
-                self.text_box.insert(tk.INSERT, '----------------\n')
+                self.text_box.insert(tk.END, "Error: Entry must be between 0V and 3000V\n")
+                self.text_box.insert(tk.END, '----------------\n')
                 self.text_box.configure(state='disabled')
             else:
-                self.r_Entry(rampV)
+                self.r_Entry(rampV)#print entry voltage to text box
                 self.text_box.configure(state='normal')
-                self.text_box.insert(tk.INSERT,'Voltage entry: ' + str(rampV) + '\n')
-                self.text_box.insert(tk.INSERT, '----------------\n')
-                self.text_box.configure(state='disabled')
-        
-        
-        
+                self.text_box.insert(tk.END,'Voltage increasing to : ' + str(rampV) + '\n')
+                self.text_box.insert(tk.END, '----------------\n')
+                self.text_box.configure(state='disabled')        
+#menu functions
     def close_window(self):
         exit()
-
-    # When ramp_Entry() is called with a valid voltage, print out info to text box
+#When ramp_Entry() is called with a valid voltage, print out info to text box
     def ramp_print_entry(self,voltage):
         self.text_box.configure(state='normal')
         self.text_box.insert(tk.END,'----------------\n')
